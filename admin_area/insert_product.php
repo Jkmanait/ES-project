fc674488f1d7664bbbad7cc958618094b7348485
jkmanait <jkmanait@gmail.com>
2024-05-21 22:44:00 +0800

    fix admin

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -1,3 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <!-- boostrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+    crossorigin="anonymous">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+    <!-- ccs file -->
+    <link rel="stylesheet" href="../style.css">
+    <style>
+        .admin_image{
+    width: 100px;
+    object-fit: contain;    
+}
+.footer{
+    position: absolute;
+    bottom: 0;
+}
+    </style>
+</head>
+<body>
+    <!-- navbar -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+            <div class="container-fluid">
+                <img src="../images/logo.jpg" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Welcome Admin</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <!-- second child -->
+        <div class="bg dark">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+
+        <!-- third child -->
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+                <div class="px-5">
+                    <a href="#"><img src="../images/admin.png" 
+                    alt="" class="admin_image"></a>
+                    <p class="text-light text-center">Admin Name</p>
+                </div>
+                <!-- button*4>a.nav-link.text-light.bg-info.my-1 -->
+                <div class="button text-center">
+                    <button class="my-3"><a href="insert_product.php" class="nav-link text-light 
+                    bg-dark my-1">Insert Products</a></button>
+                    <button><a href="index.php?insert_category" class="nav-link text-light 
+                    bg-dark my-1">Insert Categories</a></button>
+                    <button><a href="index.php?insert_brand" class="nav-link text-light 
+                    bg-dark my-1">Insert Brands</a></button> 
+                    <!-- <button><a href="" class="nav-link text-light 
+                    bg-info my-1">Logout</a></button>                  -->
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php
 include('../includes/connect.php');
 
